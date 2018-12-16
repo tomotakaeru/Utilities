1e3f2c0067e61085ca0d5ae0145c6c48d4b2eac0
tomotakaeru <38675462+tomotakaeru@users.noreply.github.com>
2018-12-16 22:49:43 +0900

    Update pdf_page_count.sh
    余分な行を削除

--- pdf_page_count.sh
+++ pdf_page_count.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# find ./ -name "[、-鶴]*.pdf" -type f -printf "%f\t\t" -exec sh -c "pdfinfo {} |grep '^Pages: ' | awk '{gsub(/^Pages:\s*/, '\s'); print $1}'" \;
-
 i=0
 sum=0
 page=()
 for path in `find ./ -name "[!0-9a-zA-Z]*.pdf" -type f`; do #頭文字が英数でない.pdfを抽出
-    page[$i]=`pdfinfo $path |grep '^Pages: ' | awk '{gsub(/^Pages:\s*/, '\s'); print $1}'`
+    page[$i]=`pdfinfo $path |grep '^Pages: ' | awk '{gsub(/^Pages:\s*/, '\s'); print $1}'`  #各.pdfの枚数を配列に格納
     file_nopath="${path##*/}"    #パスを除去
     file="${file_nopath%.*}"    #拡張子を除去
     echo -e "$file\t\t${page[$i]}枚"
